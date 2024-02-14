6598aa59c0a50934d4cd8af46a0253632c5d1750
Alejandro <alejandro.mont.cesp@gmail.com>
2024-02-14 14:50:33 -0400

    calculadora

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,22 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador"; 
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const sumarButton = document.querySelector("#sumar-button");
+const multiplicarButton = document.querySelector("#multiplicar-button");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+sumarButton.addEventListener("click", () => {
+  const firstNumber = Number(first.value);
+  const secondNumber = Number(second.value);
+  const resultado = sumar(firstNumber, secondNumber);
+  div.innerHTML = `<p>${resultado}</p>`;
+});
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+multiplicarButton.addEventListener("click", () => {
+  const firstNumber = Number(first.value);
+  const secondNumber = Number(second.value);
+  const resultado = multiplicar(firstNumber, secondNumber);
+  div.innerHTML = `<p>${resultado}</p>`;
 });
